0fff28d7973b5855dd0acba4ba43aabc55c5bdd0
ankitfs <ankitagarwal.fs@gmail.com>
2024-12-10 12:42:36 +0530

    1. Adding API-Gateway Project 2. Routing requests from api-gateway api to individual microservice

---
+++ v2-api-gateway/src/main/java/com/ankit/api/gateway/V2ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ankit.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V2ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V2ApiGatewayApplication.class, args);
+	}
+
+}
